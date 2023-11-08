b82fca8e38f065795b5f3c0f8f9faf4bbccfa8e6
unknown <it@thinksurfmedia.com>
2023-11-08 15:46:12 +0530

    5th commit

--- script.php
+++ script.php
@@ -3,5 +3,5 @@
 //Password:derdWKqa0782Z
 //https://github.com/cinecafes/cinecafes.git
 
-exec("git pull https://github.com/tsmgit/fintekin.git master");
+exec("git pull https://github.com/tsmgit/fintekin.git main");
 ?>
